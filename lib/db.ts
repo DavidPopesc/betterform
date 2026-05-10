af2553c87433c9684c3f93670135f2af55a2dea3
David Popescu <80603759+DavidPopesc@users.noreply.github.com>
2026-05-10 15:23:59 -0400

    refactor: simplify database connection logic by removing unused PostgreSQL pool

--- lib/db.ts
+++ lib/db.ts
@@ -1,24 +1,13 @@
 import { PrismaClient } from "./generated/prisma";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
 declare global {
 	// eslint-disable-next-line no-var
 	var __prisma: PrismaClient | undefined;
-	// eslint-disable-next-line no-var
-	var __pgPool: Pool | undefined;
 }
 
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-	throw new Error("DATABASE_URL is not set");
-}
-
-const pool = global.__pgPool ?? new Pool({ connectionString });
-const prisma = global.__prisma ?? new PrismaClient({ adapter: new PrismaPg(pool) });
+const prisma = global.__prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-	global.__pgPool = pool;
 	global.__prisma = prisma;
 }
 
